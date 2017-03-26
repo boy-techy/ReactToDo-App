1ec114e4800ec3251d1aa622901d2095d42df60f
yatindra Sharma <techlyf953@gmail.com>
2017-03-26 16:35:14 +0530

    Default App

---
+++ src/components/ContactUs.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Yatindra on 3/26/2017.
+ */
+
+import React,{ Component } from 'react';
+
+export default class ContactUs extends Component{
+
+    render(){
+        return(
+            <div>
+                Contact us Page
+            </div>
+        )
+    }
+}
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Yatindra on 3/26/2017.
+ */
+
+import React,{ Component } from 'react';
+
+export default class Home extends Component{
+
+    render(){
+        return(
+            <div>
+                Message From Home Component
+            </div>
+        )
+    }
+}
